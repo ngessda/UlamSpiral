a7fd5993572594c0ad089a56857d62f9cadf82bb
ngessda <shawtybae666@gmail.com>
2023-12-27 17:32:52 +0300

    none

---
+++ Ulam_Spiral/SpiralMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulam_Spiral
+{
+    internal class SpiralMatrix
+    {
+        private int[][] GetSpiralMatrix(int x, int y)
+        {
+            int[][] spiralMatrix = new int[x][y];
+            return new int[x][];
+        }
+    }
+}
---
+++ Ulam_Spiral/UlamSpiralClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulam_Spiral
+{
+    internal class UlamSpiralClass: SpiralMatrix
+    {
+        private int count = 0;
+        public int Count => count;
+        public UlamSpiralClass()
+        {
+
+        }
+    }
+}
